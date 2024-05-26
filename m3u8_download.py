7169a5391687cd06352903c89ba379bc4876dc7f
yasai-nodame <135739623+yasai-nodame@users.noreply.github.com>
2024-05-26 16:45:59 +0900

    解凍エラーをなくしてスムーズに再生し続けれるようにした

--- m3u8_download.py
+++ m3u8_download.py
@@ -4,10 +4,11 @@ import m3u8
 import os
 import aiofiles
 from moviepy.editor import VideoFileClip
+import time
 
 import write_subtitle_text
-import vlc_regeneration
 import video_path
+import file_remove 
 
 
 #############################################################
@@ -21,46 +22,44 @@ async def response_valid(url):
 
 ##########################################################
 # ライブ配信のm3u8ファイルを取得し、tsファイルを取得していく
-##########################################################
+##########################################################  # これが非同期で常に実行してるので、ファイル削除をするときにプロセス使用中とでる。　同期処理にしたらどうなるか。
 async def download_ts_segments(playlist_url, output_dir, count, m3u8_files):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(playlist_url) as response:
-            playlist = m3u8.loads(await response.text())
-            for segment in playlist.segments:
-                ts_url = segment.uri
-                if not ts_url in m3u8_files:
-                    ts_filename = f'file{count}.ts'
-                    output_path = os.path.join(output_dir, ts_filename)
-                    async with session.get(ts_url) as ts_response:
-                        async with aiofiles.open(output_path, mode='wb') as f:
-                            await f.write(await ts_response.read())
-                            print(f'Downloaded: {ts_url}')
-                        count += 1
-                        m3u8_files.append(ts_url)
-            
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(playlist_url) as response:
+                playlist = m3u8.loads(await response.text())
+                for segment in playlist.segments:
+                    ts_url = segment.uri
+                    if not ts_url in m3u8_files:
+                        ts_filename = f'file{count}.ts'
+                        output_path = os.path.join(output_dir, ts_filename)
+                        async with session.get(ts_url) as ts_response:
+                            async with aiofiles.open(output_path, mode='wb') as f:
+                                await f.write(await ts_response.read())
+                                print(f'Downloaded: {ts_url}')
+                            count += 1
+                            m3u8_files.append(ts_url)
+                            
             return count
-
+    except Exception as e:
+        print('エラーが発生しました。', e)
 
 ###################################################
 # 取得したtsファイルから音声だけを切り出して取得する
 ###################################################
 async def extract_audio_async(video_file, audio_file):
-    def extract_audio():
-        # mp4ファイルから音声を抽出して保存する
-        clip = VideoFileClip(video_file)
-        audio_clip = clip.audio
-        audio_clip.write_audiofile(audio_file)
-        
-    await asyncio.to_thread(extract_audio)
+    # tsファイルから音声を抽出して保存する
+    clip = VideoFileClip(video_file)
+    audio_clip = clip.audio
+    audio_clip.write_audiofile(audio_file)
 
 
 async def main():
     url = video_path.os.environ.get('TWICH_URL')
     count = 0 
     i = 0
+    lock = asyncio.Lock()
     
-    mkv_dir = video_path.os.environ.get('MKV_DIRECTORY')
-    srt_dir = video_path.os.environ.get('SRT_DIRECTORY')
     subtitle_srt = os.path.join(video_path.os.environ.get('SRT_DIRECTORY'), f'subtitle{i}.srt')
     
     output_dir = video_path.os.environ.get('VIDEO_FILE_PATH')
@@ -70,6 +69,14 @@ async def main():
         audio_file = os.path.join(video_path.os.environ.get('AUDIO_FILE_PATH'), f'output_audio{i}.wav')
         status_valid = await response_valid(url)
         count = await download_ts_segments(url, output_dir, count, files_name)
+        
+        try:
+            if i > 100:
+                async with lock:
+                    await file_remove.ts_remove(i - 101)
+        except Exception as e:
+            time.sleep(1)
+            print('エラーです' , e)
         
         # 動画ファイルが存在しなかったらpass、存在したら音声抽出する
         if not os.path.exists(video_file):
@@ -87,14 +94,12 @@ async def main():
                 await write_subtitle_text.transform_mkv(video_file, audio_file, subtitle_srt, i)
             else:
                 await write_subtitle_text.transform_mkv(video_file, audio_file, subtitle_srt, i)
-            vlc_regeneration.play_mkv_files(mkv_dir, srt_dir)
-        
-        
         
         
         # ライブ配信が終わったらbreakする
         if status_valid != 200:
             print('ライブ配信が終わったので、終了します。')
+            time.sleep(5)
             break 
         
         i += 1
