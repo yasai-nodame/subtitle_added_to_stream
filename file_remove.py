4fcb01a1075145c24922bec9496607d08b2dc35e
yasai-nodame <135739623+yasai-nodame@users.noreply.github.com>
2024-05-26 16:48:34 +0900

    取得して再生し終えたファイルを削除する

---
+++ file_remove.py
@@ -0,0 +1,26 @@
+import os 
+import asyncio
+import aiofiles 
+
+import video_path
+
+# ディレクトリ内の mkv ファイル,srtファイルを順番に再生
+async def file_remove(file_number):
+    tasks = []
+    loop = asyncio.get_event_loop()
+    if os.path.exists(os.path.join(video_path.os.environ.get('AUDIO_FILE_PATH'), f'output_audio{file_number}.wav')):
+        tasks.append(loop.run_in_executor(None, os.remove, os.path.join(video_path.os.environ.get('AUDIO_FILE_PATH'), f'output_audio{file_number}.wav')))
+        
+    if os.path.exists(os.path.join(video_path.os.environ.get('MKV_DIRECTORY'), f'output{file_number}.mkv')):
+        tasks.append(loop.run_in_executor(None, os.remove, os.path.join(video_path.os.environ.get('MKV_DIRECTORY'), f'output{file_number}.mkv')))
+    
+    await asyncio.gather(*tasks)
+
+async def ts_remove(file_number):
+    loop = asyncio.get_event_loop()
+    if os.path.exists(os.path.join(video_path.os.environ.get('VIDEO_FILE_PATH'), f'file{file_number}.ts')):
+        await loop.run_in_executor(None, os.remove, os.path.join(video_path.os.environ.get('VIDEO_FILE_PATH'), f'file{file_number}.ts'))
+
+
+
+
